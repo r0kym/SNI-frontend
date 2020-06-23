5e7ff15d15139772227a1ac91d9c623164bda772
r0kym <vincentvanmuys@hotmail.com>
2020-06-23 18:09:27 +0200

    Redirect to CCP login

--- homepage/views.py
+++ homepage/views.py
@@ -1,25 +1,29 @@
 from utils import SNI_URL, SNI_DYNAMIC_TOKEN
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 import requests
 
-
-# Create your views here.
 
 def home(request):
     # print(request.GET)   # access the get
     return render(request, 'home.html', {})
 
 def auth(request):
-    """Template page before there's an actual system in place"""
+    """
+    Ask SNI for a login url with eve online and then redirect to it.
+    """
 
     headers = {"Authorization": f"Bearer {SNI_DYNAMIC_TOKEN}"}
-    url = SNI_URL + "/token/use/from/dyn"
-    r = requests.post(url, headers=headers)
+    json = {"scopes": ["publicData"]}
+    url = SNI_URL + "token/use/from/dyn"
+    r = requests.post(url, headers=headers, json=json)
 
-    if r.status_code == "200":
-        return HttpResponse("It worked \\o/")
+    if r.status_code == 200:
+        return redirect(r.json()["login_url"])
     else:
-        return HttpResponse("T'rahk messed up (as usual) go and blame him")
+        return HttpResponse(f"""T'rahk messed up (as usual) go and blame him pls <br>
+        <b>error code: {r.status_code} </b><br>
+        error message: {r.json()}""")
+
