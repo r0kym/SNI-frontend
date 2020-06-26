5a837b89d8e99679b8c1d33bf14623ecca01caad
r0kym <vincentvanmuys@hotmail.com>
2020-06-26 17:48:34 +0200

    Adding a direct boostrap integration in the templates

--- SNI/settings.py
+++ SNI/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage.apps.HomepageConfig',
-    'character.apps.CharacterConfig'
+    'character.apps.CharacterConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
