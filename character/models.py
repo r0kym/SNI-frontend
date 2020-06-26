7dddef03a715914b624e89f5b9becc885c5d47d0
r0kym <vincentvanmuys@hotmail.com>
2020-06-26 18:42:13 +0200

    db model to store corporations names and id (avoid calling the esi when trying to display corporation history)

--- character/models.py
+++ character/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CorporationName(models.Model):
+    corporation_id = models.IntegerField()
+    corporation_name = models.CharField(max_length=50)  # what's a corporation name max length?
