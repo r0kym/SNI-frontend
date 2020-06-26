2b46078fe525a81706dbe59356d3efbdb2ab4712
r0kym <vincentvanmuys@hotmail.com>
2020-06-26 17:53:31 +0200

    Improves ESI

--- SNI/esi.py
+++ SNI/esi.py
@@ -9,7 +9,7 @@ import requests
 BASE_URL = "https://esi.evetech.net/latest/"
 
 
-def post_universe_names(id):
+def post_universe_names(*args):
     """
     Will send an id to the ESI and retrieve the name of the associated object
     """
@@ -21,8 +21,23 @@ def post_universe_names(id):
          "Content-Type": "application/json"
     }
 
-    data = f"[{id}]"
+    data = f"[{str(set(args))[1:-1]}]"  # removes duplicates
 
     response = requests.post(url, headers=headers, data=data)
+
+    return response
+
+def get_corporation_history(character_id):
+    """
+    Retrieve the character's corporation histoy
+    """
+
+    url = BASE_URL + f"characters/{character_id}/corporationhistory?datasource=tranquility"
+
+    headers = {
+        "accept": "application/json"
+    }
+
+    response = requests.get(url, headers=headers)
 
     return response
--- character/views.py
+++ character/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseBadRequest
 from django.views.defaults import bad_request
 
-from SNI.esi import post_universe_names
+import SNI.esi as esi
 
 # Create your views here.
 
@@ -11,8 +11,7 @@ def home(request, character_id):
     Will display the main page for accessing charachter informations
     """
 
-    request_name = post_universe_names(character_id)
-    print(request_name.json())
+    request_name = esi.post_universe_names(character_id)
     if request_name.status_code == 200:
         if request_name.json()[0]["category"] == "character":
             character_name = request_name.json()[0]["name"]
