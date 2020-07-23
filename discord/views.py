44d4a9d92fe4eaedff2899664f7cd658050b7a7d
r0kym <vincentvanmuys@hotmail.com>
2020-07-23 16:03:15 +0200

    Sends error message if the discord module hasn't been activate in the backend

--- discord/views.py
+++ discord/views.py
@@ -23,11 +23,14 @@ def home(request):
 
     request_auth_code = requests.post(url, headers=headers)
 
-    if request_auth_code.status_code != 200:
+    if request_auth_code.status_code == 200:
+        return render(request, 'discord/home.html', {
+            "request_code": request_auth_code.json()
+        })
+
+    elif request_auth_code.status_code == 404:
+        return render(request, 'discord/notactive.html')
+    else:
         return HttpResponse(f"""
         ERROR {request_auth_code.status_code} <br>
         {request_auth_code.json()}""")
-
-    return render(request, 'discord/home.html', {
-        "request_code": request_auth_code.json()
-    })
