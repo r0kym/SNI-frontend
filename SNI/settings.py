41eb8a4b502b6dad3c86d81149ab8f93859ee033
r0kym <vincentvanmuys@hotmail.com>
2020-07-23 16:06:55 +0200

    Moved ALLOWED_HOSTS in utils.py

--- SNI/settings.py
+++ SNI/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True   go to utils.py to modify it
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [] go in utils.py to modify it
 
 
 # Application definition
