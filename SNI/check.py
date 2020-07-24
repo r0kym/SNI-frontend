d05b8b5f6527cd55d67bd53f27f5961f68060fce
r0kym <vincentvanmuys@hotmail.com>
2020-07-24 17:36:05 +0200

    Patch check_tokens method

--- SNI/check.py
+++ SNI/check.py
@@ -8,34 +8,32 @@ from utils import SNI_URL
 from django.shortcuts import redirect
 
 import requests
+from functools import wraps
 
 
-def check_tokens(view, min_clearance=0):
+def check_tokens(min_clearance=0):
     """
     Will check if there's a user token in the browser's cookies before executing the function and will redirect to the
     homepage if there's none
 
     min_clearance: minimal clearance needed to load the page.
     """
+    def decorator(view):
+        @wraps(view)
+        def inner(request, *args, **kwargs):
+            if (token := request.session.get("user_token")):
+                if min_clearance != 0:
+                    url = SNI_URL + f"user/{request.session['user_id']}"
+                    headers = {
+                        "accept": "application/json",
+                        "Authorization": f"Bearer {token}"
+                    }
 
-    def wrapper(request, *args, **kwargs):
+                    request_user = requests.get(url, headers=headers)
 
-        if (token := request.session.get("user_token")):
-
-            if min_clearance != 0:
-                url = SNI_URL + f"user/{request.session['user_id']}"
-                headers = {
-                    "accept": "application/json",
-                    "Authorization": f"Bearer {token}"
-                }
-
-                request_user = requests.get(url, headers=headers)
-
-                if request_user.json()["clearance_level"] < min_clearance:
-                    redirect('no-premission')
-
-            return view(request, *args, **kwargs)
-        else:
+                    if request_user.json()["clearance_level"] < min_clearance:
+                        return redirect('no-premission')
+                return view(request, *args, **kwargs)
             return redirect("/")
-
-    return wrapper
+        return inner
+    return decorator
