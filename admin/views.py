85165eb57f083069fa81e5514e94d83ca8d44dfa
r0kym <vincentvanmuys@hotmail.com>
2020-07-24 17:36:36 +0200

    Method to submit a new job to the scherduler

--- admin/views.py
+++ admin/views.py
@@ -10,9 +10,10 @@ from SNI.error import render_error
 from utils import SNI_URL
 
 import requests
+from urllib.parse import urlencode
 
 
-#@check_tokens(10)
+@check_tokens(10)
 def home(request):
     """
     Home view for the administration part
@@ -32,3 +33,27 @@ def home(request):
     return render(request, 'admin/home.html', {
         "jobs": request_jobs.json(),
     })
+
+@check_tokens(10)
+def submit(request):
+    """
+    Submits a job to the scheduler
+    """
+
+    callable_name = request.POST["callable_name"]
+    url = SNI_URL + f"system/job/{callable_name}"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {request.session['user_token']}"
+    }
+
+    request_submit = requests.post(url, headers=headers)
+
+    if request_submit.status_code != 200:
+        return render_error(request_submit)
+
+    params = urlencode({'sub_job': callable_name})
+    return_url = reverse("admin-home") + "?" + params
+
+    return redirect(return_url)
