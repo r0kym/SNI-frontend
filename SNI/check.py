b64f3c7c245c050c8603ec61195629ee4f938c3c
r0kym <vincentvanmuys@hotmail.com>
2020-07-24 15:57:59 +0200

    Add an attribute to block a view based on the clearance of the user

--- SNI/check.py
+++ SNI/check.py
@@ -2,18 +2,38 @@
 Containns commands used to check user accesses to different pages
 """
 
+
+from utils import SNI_URL
+
 from django.shortcuts import redirect
 
+import requests
 
-def check_tokens(view):
+
+def check_tokens(view, min_clearance=0):
     """
     Will check if there's a user token in the browser's cookies before executing the function and will redirect to the
     homepage if there's none
+
+    min_clearance: minimal clearance needed to load the page.
     """
 
     def wrapper(request, *args, **kwargs):
 
-        if request.session.get("user_token"):
+        if (token := request.session.get("user_token")):
+
+            if min_clearance != 0:
+                url = SNI_URL + f"user/{request.session['user_id']}"
+                headers = {
+                    "accept": "application/json",
+                    "Authorization": f"Bearer {token}"
+                }
+
+                request_user = requests.get(url, headers=headers)
+
+                if request_user.json()["clearance_level"] < min_clearance:
+                    redirect('no-premission')
+
             return view(request, *args, **kwargs)
         else:
             return redirect("/")
