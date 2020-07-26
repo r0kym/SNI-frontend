9b7b49f465a324b0519147023dd651dec4132c8c
Fabien <fabienmazieres@yahoo.fr>
2020-07-26 11:02:55 +0100

    Remove sesson if token is not valid

--- SNI/error.py
+++ SNI/error.py
@@ -1,9 +1,18 @@
 import requests, json
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied, SuspiciousOperation
 
 def render_error(request):
     if request.status_code == 403:
         raise PermissionDenied
+    elif request.status_code == 401:
+        try:
+            del request.session['user_token']
+        except KeyError:
+            pass
+
+        # response.delete_cookie('user_token')
+        return redirect("/")
     else:
         return HttpResponse(f"""<pre>ERROR {request.status_code}<br>{json.dumps(request.json(), indent=1)}</pre>""")
