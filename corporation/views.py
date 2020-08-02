b5094c6a19d3e581136ee0501ab29461ec6b5bf2
r0kym <vincentvanmuys@hotmail.com>
2020-08-02 12:06:25 +0200

    Addition of corporation navbar

--- corporation/views.py
+++ corporation/views.py
@@ -19,7 +19,7 @@ def home(request):
 
     request_corp = requests.get(GLOBAL_URL, headers=global_headers(request))
     if request_corp.status_code != 200:
-        render_error(request_corp)
+        return render_error(request_corp)
 
     return render(request, "corporation/home.html", {
         "corporations": request_corp.json(),
@@ -33,16 +33,17 @@ def sheet(request, corp_id):
 
     request_corp = requests.get(GLOBAL_URL+f"/{corp_id}", headers=global_headers(request))
     if request_corp.status_code != 200:
-        render_error(request_corp)
+        return render_error(request_corp)
 
     esi_request_corp = get_corporations_corporation_id(corp_id)
     if esi_request_corp.status_code != 200:
-        render_error(esi_request_corp)
+        return render_error(esi_request_corp)
 
     return render(request, "corporation/sheet.html", {
         "corporation": request_corp.json(),
         "esi": esi_request_corp.json(),
-        "corporation_id": corp_id
+        "corporation_id": corp_id,
+        "corporation_name": esi_request_corp.json()["name"],
     })
 
 @check_tokens(1)
@@ -56,6 +57,14 @@ def tracking(request, corp_id):
     if request_track.status_code != 200:
         return render_error(request_track)
 
+    request_corp = get_corporations_corporation_id(corp_id)
+    if request_corp.status_code != 200:
+        return render_error(request_corp)
+
+
     return render(request, "corporation/tracking.html", {
         "tracking": request_track.json(),
+        "corporation_id": corp_id,
+        "corporation_name": request_corp.json()["name"],
+
     })
