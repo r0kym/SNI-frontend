8049159f14e1ea8d351158c850c2fc45ebbf4599
r0kym <vincentvanmuys@hotmail.com>
2020-08-02 13:01:09 +0200

    Modify corporation scopes

--- corporation/urls.py
+++ corporation/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.home, name='corporation-home'),
     path('<int:corp_id>/', views.sheet, name='corporation-sheet'),
     path('<int:corp_id>/tracking', views.tracking, name='corporation-tracking'),
+    path('<int:corp_id>/scopes', views.change_scopes, name='corpoation-change-scopes'),
 ]
--- corporation/views.py
+++ corporation/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 
 from SNI.check import check_tokens
 from SNI.error import render_error
-from SNI.esi import post_universe_names, get_corporations_corporation_id
+from SNI.esi import post_universe_names, get_corporations_corporation_id, ESI_SCOPES
 from SNI.lib import global_headers
 from utils import SNI_URL
 
 import requests
+from urllib.parse import urlencode
 
 
 GLOBAL_URL = SNI_URL + "corporation"
+
 
 @check_tokens(1)
 def home(request):
@@ -44,6 +46,8 @@ def sheet(request, corp_id):
         "esi": esi_request_corp.json(),
         "corporation_id": corp_id,
         "corporation_name": esi_request_corp.json()["name"],
+        "scopes": ESI_SCOPES,
+        "changed_scopes": request.GET.get("changed_scopes"),
     })
 
 @check_tokens(1)
@@ -66,5 +70,27 @@ def tracking(request, corp_id):
         "tracking": request_track.json(),
         "corporation_id": corp_id,
         "corporation_name": request_corp.json()["name"],
-
     })
+
+@check_tokens(4)
+def change_scopes(request, corp_id):
+    """
+    Changing corporation mandatory scopes
+    """
+
+    scopes = []
+    for key in request.POST:
+        if key in ESI_SCOPES:
+            scopes.append(key)
+
+    if len(scopes) > 0:
+        data = "{\"mandatory_esi_scopes\": [\"" + "\",\"".join(scopes) + "\"]}"
+    else:
+        data = "{\"mandatory_esi_scopes\": []}"
+    request_change = requests.put(GLOBAL_URL+f"/{corp_id}", headers=global_headers(request, {"Content-type":"application/json"}), data=data)
+    if request_change.status_code != 200:
+        return render_error(request_change)
+
+    params = urlencode({"changed_scopes": "true"})
+    return_url = reverse("corporation-home") + str(corp_id) + "?" + params
+    return redirect(return_url)
