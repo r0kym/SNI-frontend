e5752dc0fc176b41e0e68c145c6a3820f2a8f2bb
Fabien <fabienmazieres@yahoo.fr>
2020-08-17 20:45:16 +0100

    Add fallback for station vs structure id

--- character/templatetags/id2name.py
+++ character/templatetags/id2name.py
@@ -11,6 +11,11 @@ def esi_name(id, route=None, request=None):
     Will use the model IdToName to get the name of something
     """
     try:
-        return IdToName.get_name(id, route, request)
+        name = IdToName.get_name(id, route, request)
+        if "Couldn't find" in name and route == "universe/stations":
+            name = IdToName.get_name(id, "universe/structures", request)
+        elif "Couldn't find" in name and route == "universe/structures":
+            name = IdToName.get_name(id, "universe/stations", request)
+        return name
     except:
         return f"Error when loading the name of {id}"
